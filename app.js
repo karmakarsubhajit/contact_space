d7505328624a8147735abfba5ea091383bc08b78
karmakarsubhajit <111801041@smail.iitpkd.ac.in>
2023-01-26 19:46:52 +0530

    Making changes

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const people_contacts = require('./model/contacts');
 
 app.get('/',(req,res)=>{
-    res.send('Hello World!!!');
+    res.send('Welcome to our page!!! You can store your contacts!');
+});
+
+app.get('/contacts',(req,res)=>{
+    res.send(people_contacts);
 });
 
 app.listen(port,function(){
---
+++ model/contacts.js
@@ -0,0 +1,5 @@
+const people_contacts = {
+    "Samrat":9807623451,"Soumyajit":9081234092,"Bisruto":8710283746
+};
+
+module.exports = people_contacts;
